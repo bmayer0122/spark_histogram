7d31b057fb785d35c6c5e70039730a31fccec290
Benjamin Mayer <bmayer@gmail.com>
2016-06-11 18:56:43 -0400

    completed run; cleaned up bar_stacked

--- histogram/bar_stacked.py
+++ histogram/bar_stacked.py
@@ -2,21 +2,28 @@
 # a stacked bar plot with errorbars
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+from ast import literal_eval as make_tuple
 
-array = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-N = len(array)
-count = [8, 11, 14, 11, 6, 11, 10, 11, 6, 8]
+f = open('histogram.txt')
+line = f.readline()
+tup = make_tuple(line)
+index = tup[0]
+count = tup[1]
+
+N = len(index)-1
 ind = np.arange(N)    # the x locations for the groups
 width = 0.35       # the width of the bars: can also be len(x) sequence
 
 
+tickSize = max(count)/10
 #p1 = plt.bar(ind, menMeans, width, color='r', yerr=menStd)
 p1 = plt.bar(ind, count, width, color='r')
 
 plt.ylabel('Count')
 plt.title('Count by integer')
-plt.xticks(ind + width/2., array)
-plt.yticks(np.arange(0, max(count)+10, 10))
-plt.legend((p1[0], None), ('Men', None))
+plt.xticks(ind + width/2., index)
+plt.yticks(np.arange(0, max(count)+tickSize, tickSize))
+plt.legend((p1[0], None), ('Count', None))
 
 plt.show()
