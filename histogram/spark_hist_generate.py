3aa027cc290818d3407a6a8d287d15e95a0f1398
Benjamin Mayer <bmayer@gmail.com>
2016-06-11 17:21:10 -0400

    spark generates histogram and writes to text file

---
+++ histogram/spark_hist_generate.py
@@ -0,0 +1,14 @@
+from pyspark import SparkContext
+
+logFile = "~/projects/spark/histogram/README.md"  # Should be some file on your system
+sc = SparkContext("local", "Histogram")
+logData = sc.textFile(logFile).cache()
+
+nums = sc.parallelize(range(0,10))
+oversample = nums.sample(True, 1000)
+#oversample.count()
+f = open('histogram.txt', 'w')
+f.write(str(oversample.histogram(range(0,10))))
+f.close()
+
+
